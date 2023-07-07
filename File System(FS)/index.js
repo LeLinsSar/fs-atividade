48b430f1f7f985373cbba2e1ea0d2aa9dfcbb894
Letícia <110118449+LeLinsSar@users.noreply.github.com>
2023-07-07 15:35:30 -0300

    Add files via upload

---
+++ File System(FS)/index.js
@@ -0,0 +1,34 @@
+// atividade 1:
+
+//const fs = require('fs');
+
+//const port = 3000;
+
+//fs.readFile('/Users/leticialins/test.txt', 'UTF8', (err, data) =>{
+    //if(err) {
+      //  console.log(err);
+      //  return
+    //}
+    //console.log(data);
+//});
+//server.listen(port, () =>{
+    //console.log(`Servidor rodando na porta ${port}`);
+//});
+
+//atividade 2:
+//const fs = require('fs')
+
+//const port = 3000
+
+//const data = 'Testando a criação de arquivos';
+//fs.writeFile('arquivo.txt', data, (err) => {
+   // if (err) throw err;
+ // console.log('O arquivo foi criado!');
+//});
+
+//server.listen(port, () =>{
+    //console.log(`Servidor rodando na porta ${port}`);
+//});
+
+//atividade 3:
+
